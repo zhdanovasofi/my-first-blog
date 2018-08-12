c0b7b5fd6e96f2cfd15805ca4636a10444fc6d17
Sofia <sofya.zhdanova.97@mail.ru>
2018-08-12 18:26:22 +0300

    Some new pages for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
